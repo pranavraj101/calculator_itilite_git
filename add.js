3611f689b12964e4e725b1aed998172d76fd9c9b
pranavraj101 <sinhapranavraj10142@gmail.com>
2022-09-28 14:18:52 +0530

    Javascript file

---
+++ add.js
@@ -0,0 +1,9 @@
+function add(){
+
+    var num1=document.getElementById("firstnum").value
+    var num2=document.getElementById("secondnum").value
+
+    var result=num1+num2
+
+    document.getElementById("result").value=result
+}
