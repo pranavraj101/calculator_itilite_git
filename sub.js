285b3cf7c3b6a18ec195a834a2587a4085285ce1
pranavraj101 <sinhapranavraj10142@gmail.com>
2022-09-28 14:49:10 +0530

    substraction funcrtionality ADDED

--- sub.js
+++ sub.js
@@ -1,9 +1,9 @@
-function add(){
+function sub(){
 
     var num1=document.getElementById("firstnum").value
     var num2=document.getElementById("secondnum").value
 
-    var result=parseInt(num1)+parseInt(num2)
+    var result=parseInt(num1)-parseInt(num2)
 
     document.getElementById("result").value=result
 }
